6cc95dfb73f9c8a3cced329275ebe1cadc92f8f6
Hicham Bouzalim <bouzalimhicham11@gmail.com>
2025-07-24 11:41:42 +0200

    updated 24/07/2025

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Chatbot",
+    version="0.1.0",
+    author="hicham bouzalim",
+    author_email="bouzalim",
+    packages=find_packages(),
+    install_requires=[]
+)
